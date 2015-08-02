bd49513110e5108c4836c6170c2bfd4e7e10f27d
andy <andy@andydote.co.uk>
2015-08-02 20:34:11 +0100

    tests: add test project for overseer

---
+++ Overseer.Tests/Scratchpad.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using Overseer.RabbitMQ;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Overseer.Tests
+{
+	public class Scratchpad
+	{
+		private readonly ITestOutputHelper _output;
+
+		public Scratchpad(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void When_testing_something()
+		{
+		}
+	}
+}
