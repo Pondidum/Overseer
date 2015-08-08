30b86467c9dda41aa361b5cb692ceb56a6bc33a8
andy <andy@andydote.co.uk>
2015-08-08 15:18:16 +0100

    validatorSource: add cachingValidatorSource

---
+++ Overseer.Tests/Sources/CachingValidatorSourceTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using NSubstitute;
+using Overseer.Sources;
+using Shouldly;
+using Xunit;
+
+namespace Overseer.Tests.Sources
+{
+	public class CachingValidatorSourceTests
+	{
+		private readonly IValidatorSource _other;
+		private readonly CachingValidatorSource _cached;
+
+		public CachingValidatorSourceTests()
+		{
+			_other = Substitute.For<IValidatorSource>();
+			_cached = new CachingValidatorSource(_other);
+		}
+		[Fact]
+		public void When_the_other_source_is_null()
+		{
+			Should.Throw<ArgumentNullException>(() => new CachingValidatorSource(null));
+		}
+
+		[Fact]
+		public void When_fetching_a_validator()
+		{
+			var validator = Substitute.For<IValidator>();
+
+			_other.For(Arg.Any<string>()).Returns(new[] { validator });
+
+			_cached.For("test").ShouldBe(new[] { validator });
+			_other.Received(1).For("test");
+		}
+
+		[Fact]
+		public void When_fetching_a_validator_multiple_times()
+		{
+			_other.For(Arg.Any<string>()).Returns(Enumerable.Empty<IValidator>());
+
+			_cached.For("test").ShouldBeEmpty();
+			_cached.For("test").ShouldBeEmpty();
+
+			_other.Received(1).For("test");
+		}
+	}
+}
---
+++ Overseer/Sources/CachingValidatorSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Overseer.Sources
+{
+	public class CachingValidatorSource : IValidatorSource
+	{
+		private readonly object _locker;
+		private readonly Dictionary<string, List<IValidator>> _cache;
+		private readonly Func<string, List<IValidator>> _onMissing;
+
+		public CachingValidatorSource(IValidatorSource other)
+		{
+			if (other== null) throw new ArgumentNullException("other");
+
+			_locker = new object();
+			_cache = new Dictionary<string, List<IValidator>>();
+			_onMissing = key => other.For(key).ToList();
+		}
+
+		public IEnumerable<IValidator> For(string messageType)
+		{
+			Fill(messageType);
+			return _cache[messageType];
+		}
+		
+		public void Fill(string key)
+		{
+			if (_cache.ContainsKey(key))
+			{
+				return;
+			}
+
+			lock (_locker)
+			{
+				if (!_cache.ContainsKey(key))
+				{
+					_cache.Add(key, _onMissing(key));
+				}
+			}
+		}
+	}
+}
