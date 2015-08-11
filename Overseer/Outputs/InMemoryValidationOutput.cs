f629278338138e1c4f7ce66040fb24256f6b2528
andy <andy@andydote.co.uk>
2015-08-11 17:31:43 +0100

    outputs: add inMemoryValidationOutput

---
+++ Overseer/Outputs/InMemoryValidationOutput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Overseer.Outputs
+{
+	public class InMemoryValidationOutput : IValidationOutput
+	{
+		private readonly List<ValidationResult> _results;
+
+		public InMemoryValidationOutput()
+		{
+			_results = new List<ValidationResult>();
+		}
+
+		public IEnumerable<ValidationResult> Results { get { return _results; } }
+ 
+		public void Write(ValidationResult result)
+		{
+			_results.Add(result);
+		}
+	}
+}
