a803df4b8c0ef52815b4b83b6809dacfbf22e93b
andy <andy@andydote.co.uk>
2015-08-12 22:58:29 +0100

    overseer: add inMemoryMessageReader

--- Overseer.Tests/Acceptance/AcceptanceTests.cs
+++ Overseer.Tests/Acceptance/AcceptanceTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using NSubstitute;
 using Overseer.Outputs;
 using Overseer.RabbitMQ;
+using Overseer.Readers;
 using Overseer.Validators;
 using Shouldly;
 using Xunit;
@@ -11,30 +12,26 @@ namespace Overseer.Tests.Acceptance
 {
 	public class AcceptanceTests
 	{
-
-		private Action<object> _pushMessage;
-		private InMemoryValidationOutput _output;
+		private readonly InMemoryMessageReader _messages;
+		private readonly InMemoryValidationOutput _output;
 
 		public AcceptanceTests()
 		{
-			var reader = Substitute.For<IMessageReader>();
-
-			reader.When(r => r.Start(Arg.Any<Action<object>>())).Do(c => _pushMessage = c.Arg<Action<object>>());
+			_messages = new InMemoryMessageReader();
+			_output = new InMemoryValidationOutput();
 
 			var converter = Substitute.For<IMessageConverter>();
 			var source = Substitute.For<IValidatorSource>();
 			var validator = new MessageValidator(source);
 
-			_output = new InMemoryValidationOutput();
-
-			var queueMonitor = new MonitorQueue(reader, converter, validator, _output);
+			var queueMonitor = new MonitorQueue(_messages, converter, validator, _output);
 			queueMonitor.Start();
 		}
 
 		[Fact]
 		public void When_reading_message_which_cannot_be_converted()
 		{
-			_pushMessage("testing");
+			_messages.Push("Testing");
 
 			_output.Results.Single().Status.ShouldBe(Status.Warning);
 			_output.Results.Single().Message.ShouldBe("Unable to convert message of type String using IMessageConverterProxy.");
---
+++ Overseer/Readers/InMemoryMessageReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Overseer.Readers
+{
+	public class InMemoryMessageReader : IMessageReader
+	{
+		private Action<object> _pushMessage;
+
+		public InMemoryMessageReader()
+		{
+			Stop();
+		}
+
+		public void Push(object message)
+		{
+			_pushMessage(message);
+		}
+
+		public void Start(Action<object> onMessage)
+		{
+			_pushMessage = onMessage;
+		}
+
+		public void Stop()
+		{
+			_pushMessage = message => { };
+		}
+	}
+}
