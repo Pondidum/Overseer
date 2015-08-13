ee8c1c18408eb29e01a97798b2f88d83f27df866
andy <andy@andydote.co.uk>
2015-08-13 22:30:40 +0100

    cachingValidator: extract cache as a separate class

---
+++ Overseer/Infrastructure/Cache.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Overseer.Infrastructure
+{
+	public class Cache<TKey, TValue> : IEnumerable<TValue>
+	{
+		private readonly Func<TKey, TValue> _onMissing;
+		private readonly object _locker = new object();
+		private readonly Dictionary<TKey, TValue> _cache;
+
+		public Cache(Func<TKey, TValue> onMissing)
+			: this(null, onMissing)
+		{
+		}
+
+		public Cache(IEqualityComparer<TKey> comparer, Func<TKey, TValue> onMissing)
+		{
+			_onMissing = onMissing;
+			_cache = new Dictionary<TKey, TValue>(comparer);
+		}
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				Fill(key);
+				return _cache[key];
+			}
+		}
+
+		public void Fill(TKey key)
+		{
+			if (_cache.ContainsKey(key))
+			{
+				return;
+			}
+
+			lock (_locker)
+			{
+				if (!_cache.ContainsKey(key))
+				{
+					_cache.Add(key, _onMissing(key));
+				}
+			}
+		}
+
+		public IEnumerator<TValue> GetEnumerator()
+		{
+			return _cache.Values.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
--- Overseer/Sources/CachingValidatorSource.cs
+++ Overseer/Sources/CachingValidatorSource.cs
@@ -1,44 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Overseer.Infrastructure;
 
 namespace Overseer.Sources
 {
 	public class CachingValidatorSource : IValidatorSource
 	{
-		private readonly object _locker;
-		private readonly Dictionary<string, List<IValidator>> _cache;
-		private readonly Func<string, List<IValidator>> _onMissing;
+		private readonly Cache<string, List<IValidator>> _cache;
 
 		public CachingValidatorSource(IValidatorSource other)
 		{
 			if (other== null) throw new ArgumentNullException("other");
 
-			_locker = new object();
-			_cache = new Dictionary<string, List<IValidator>>();
-			_onMissing = key => other.For(key).ToList();
+			_cache = new Cache<string, List<IValidator>>(
+				StringComparer.OrdinalIgnoreCase, 
+				key => other.For(key).ToList());
 		}
 
 		public IEnumerable<IValidator> For(string messageType)
 		{
-			Fill(messageType);
 			return _cache[messageType];
-		}
-		
-		public void Fill(string key)
-		{
-			if (_cache.ContainsKey(key))
-			{
-				return;
-			}
-
-			lock (_locker)
-			{
-				if (!_cache.ContainsKey(key))
-				{
-					_cache.Add(key, _onMissing(key));
-				}
-			}
 		}
 	}
 }
