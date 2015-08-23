fc4d5ac234190bc9b684811b6c9b8fea07402548
andy <andy@andydote.co.uk>
2015-08-23 20:20:38 +0100

    validationSources: add resourceValidationSource

---
+++ Overseer.Tests/Sources/ResourceValidatorSourceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Overseer.Sources;
+using Shouldly;
+using Xunit;
+
+namespace Overseer.Tests.Sources
+{
+	public class ResourceValidatorSourceTests
+	{
+		private readonly string _pattern;
+
+		public ResourceValidatorSourceTests()
+		{
+			_pattern = "Overseer.Tests.Resources." + ResourceValidatorSource.MessageTypeReplacementTag + ".json";
+		}
+
+		[Fact]
+		public void When_reading_a_valid_resource()
+		{
+			var source = new ResourceValidatorSource(GetType().Assembly, _pattern);
+			source.For("PersonExactMatch").ShouldNotBeEmpty();
+		}
+
+		[Fact]
+		public void When_reading_a_multi_source_resource()
+		{
+			var source = new ResourceValidatorSource(GetType().Assembly, "Overseer.Tests.Resources.multispec.json");
+			source.For("PersonExactMatch").Count().ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_reading_an_invalid_resouce()
+		{
+			var source = new ResourceValidatorSource(GetType().Assembly, _pattern);
+			source.For("wefwefwf").ShouldBeEmpty();
+		}
+	}
+}
---
+++ Overseer/Sources/ResourceValidatorSource.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using Overseer.Validators;
+
+namespace Overseer.Sources
+{
+	public class ResourceValidatorSource : IValidatorSource
+	{
+		public const string MessageTypeReplacementTag = "{messageType}";
+
+		private readonly Assembly _assembly;
+		private readonly string _filePattern;
+
+		public ResourceValidatorSource(Assembly assembly, string filePattern)
+		{
+			_assembly = assembly;
+			_filePattern = filePattern;
+		}
+
+		private string GetFilename(string messageType)
+		{
+			return _filePattern.Replace(MessageTypeReplacementTag, messageType);
+		}
+
+		public IEnumerable<IValidator> For(string messageType)
+		{
+			var fullname = GetFilename(messageType);
+
+			try
+			{
+				using (var stream = _assembly.GetManifestResourceStream(fullname))
+				using (var reader = new StreamReader(stream))
+				{
+					var json = reader.ReadToEnd();
+					var token = JToken.Parse(json);
+
+					var validators = token.Type == JTokenType.Array
+						? token.ToObject<IEnumerable<JsonSchemaValidator>>()
+						: new[] { token.ToObject<JsonSchemaValidator>() };
+
+					return validators
+						.Where(v => string.Equals(v.Type, messageType));
+				}
+			}
+			catch (Exception)
+			{
+				return Enumerable.Empty<IValidator>();
+			}
+		}
+	}
+}
