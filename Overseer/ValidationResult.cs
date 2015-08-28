37875cec097b7dfb8c193e59abdc2dcafb8681fe
andy <andy@andydote.co.uk>
2015-08-28 22:38:37 +0100

    validationResult: split types into separate files

--- Overseer/ValidationResult.cs
+++ Overseer/ValidationResult.cs
@@ -18,27 +18,4 @@ namespace Overseer
 			Status = children.Select(c => c.Status).DefaultIfEmpty(Status.Pass).Max();
 		}
 	}
-
-	public class ValidationNode
-	{
-		public Status Status { get; protected set; }
-		public string ValidationMessage { get; protected set; }
-		public IEnumerable<ValidationNode> Children { get; protected set; }
-
-		public ValidationNode(Status status, string validationMessage)
-		{
-			Status = status;
-			ValidationMessage = validationMessage;
-			Children = Enumerable.Empty<ValidationNode>();
-		}
-
-		public ValidationNode(string validationMessage, IEnumerable<ValidationNode> nodes)
-		{
-			var children = nodes.ToList();
-
-			ValidationMessage = validationMessage;
-			Children = children;
-			Status = children.Select(c => c.Status).DefaultIfEmpty(Status.Pass).Max();
-		}
-	}
 }
